20a3b4df7979ac3dd806865886e43a55bb98527b
buidaithanh <84963463+buidaithanh@users.noreply.github.com>
2023-03-03 22:50:19 +0700

    Update constains.js

--- client/src/contexts/constains.js
+++ client/src/contexts/constains.js
@@ -1,1 +1,5 @@
 export const LOCAL_STORAGE_TOKEN_KEY = "pet-shop";
+export const apiUrl =
+  process.env.NODE_ENV === "production"
+    ? "http://localhost:5000/api"
+    : "https://testingg-backend.onrender.com/api";
