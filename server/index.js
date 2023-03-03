da14342c92d1c709dede761aab4b2c3f531b0af4
dai thanh <thanh0985837774@gmail.com>
2023-03-03 20:51:12 +0700

    update

--- server/index.js
+++ server/index.js
@@ -28,6 +28,6 @@ app.use(cors());
 app.use("/api/auth", authRouter);
 app.use("/api/product", productRouter);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`sever run build on ${PORT}`));
